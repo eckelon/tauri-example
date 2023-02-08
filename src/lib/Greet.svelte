98198bbbd06660a6d3ee20887c4d75f8f857e972
Jesus Samitier <contacto@jasamitier.com>
2023-02-09 00:29:29 +0100

    First commit

---
+++ src/lib/Greet.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { invoke } from "@tauri-apps/api/tauri";
+  let name = "";
+  let greetMsg = "";
+
+  async function greet_on_key({ key }) {
+    if (key !== 'Enter') {
+      return;
+    }
+
+    greetMsg = await invoke("greet", { name });
+  }
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    greetMsg = await invoke("greet", { name });
+  }
+</script>
+
+<div>
+  <div class="row">
+    <input id="greet-input" placeholder="Enter a name..." bind:value={name}  on:keypress={greet_on_key}/>
+    <button on:click={greet} > Greet </button>
+  </div>
+  <p>{greetMsg}</p>
+</div>
